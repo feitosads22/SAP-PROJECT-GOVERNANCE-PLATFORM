f12404e521feee97cf074f1d0cf7a95a1fcfaa60
Filipe Feitosa <107809755+feitosads22@users.noreply.github.com>
2026-09-06 13:20:58 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import ProjectDetail from './pages/ProjectDetail'
 import MyTasks from './pages/MyTasks'
 import Capacity from './pages/Capacity'
 import TimesheetPage from './pages/TimesheetPage'
+import BudgetPage from './pages/BudgetPage'
 import NavBar from './components/NavBar'
 
 function ErroConfiguracao() {
@@ -46,7 +47,9 @@ function App() {
         <Route path="/projeto/:id"   element={<ProjectDetail role={role} userId={userId} />} />
         <Route path="/minhas"        element={<MyTasks role={role} userId={userId} />} />
         <Route path="/capacidade"     element={<Capacity />} />
+        <Route path="/projeto/:id/financeiro" element={<BudgetPage role={role} userId={userId} />} />
         <Route path="/timesheet"      element={<TimesheetPage role={role} userId={userId} />} />
+        <Route path="/projeto/:id/financeiro" element={<BudgetPage role={role} userId={userId} />} />
         <Route path="*"              element={<Navigate to="/" replace />} />
       </Routes>
     </>

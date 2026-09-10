d0d4171d980aa5ed3d3873bcddb6fe1624ecb9dc
Filipe Feitosa <107809755+feitosads22@users.noreply.github.com>
2026-09-09 23:36:08 -0300

    Update app.types.ts

--- src/types/app.types.ts
+++ src/types/app.types.ts
@@ -1,6 +1,11 @@
 import type { Database } from './database.types'
 
-export type Task = Database['public']['Tables']['tasks']['Row']
+export type Task = Database['public']['Tables']['tasks']['Row'] & {
+  start_date?: string | null
+  due_date?: string | null
+  progress?: number
+  description?: string | null
+}
 export type Project = Database['public']['Tables']['projects']['Row']
 export type Profile = Database['public']['Tables']['profiles']['Row']
 export type TaskEvidence = Database['public']['Tables']['task_evidences']['Row']
